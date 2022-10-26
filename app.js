48eee326c3cd5de743f6f3c98043861ccb41edcf
optimisticlucifer <b20314@students.iitmandi.ac.in>
2022-10-27 01:44:05 +0530

    add put route for stories

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
-const path= require('path')
-const express =require('express')
+const path = require('path')
+const express = require('express')
 const mongoose = require('mongoose')
-const dotenv =require('dotenv')
-const morgan=require('morgan')
-const exphbs= require('express-handlebars')
-const passport=require('passport')
-const session=require('express-session')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 
 
 //Load config
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 //Passport config
 require('./config/passport')(passport)
 
 connectDB()
 
-const app =express()
+const app = express()
 
 //Body Parser
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+//method overrride
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 //Logging
-if(process.env.NODE_ENV==='development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 //handlebars Helpers
-const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 //Handlerbars
 app.engine('.hbs', exphbs.engine({
@@ -42,7 +53,7 @@ app.engine('.hbs', exphbs.engine({
         editIcon,
         select
     },
-    defaultLayout:'main',
+    defaultLayout: 'main',
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
@@ -54,7 +65,7 @@ app.use(session({
     saveUninitialized: false,
     // cookie: { secure: true },
     //session in mongodb
-    store: MongoStore.create({mongoUrl: process.env.MONGO_URI,}),
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, }),
 }))
 
 //PassPort middleware
@@ -62,23 +73,23 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 //Set global var
-app.use(function (req,res,next){
+app.use(function (req, res, next) {
     res.locals.user = req.user || null
     next()
 })
 
 
 //Static folder
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
-app.use('/',require('./routes/index'))
-app.use('/auth',require('./routes/auth'))
-app.use('/stories',require('./routes/stories'))
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 
-const PORT =process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })

0f2c96c65baca4e7c85e748e0ac165bace56cb04
optimisticlucifer <b20314@students.iitmandi.ac.in>
2022-10-27 01:55:45 +0530

    add delete route feature

--- routes/stories.js
+++ routes/stories.js
@@ -23,8 +23,6 @@ router.post('/',ensureAuth,async (req,res)=>{
     }
 })
 
-
-
 // @desc Show all stories
 // @route GET /stories
 router.get('/', ensureAuth , async (req,res)=>{
@@ -44,20 +42,25 @@ router.get('/', ensureAuth , async (req,res)=>{
 // @desc Show edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth ,async (req,res)=>{
-    const story = await Story.findOne({
-        _id: req.params.id
-    }).lean()
-
-    if(!story){
-        return res.render('error/404')
-    }
-
-    if(story.user != req.user.id){
-        res.redirect('/stories')
-    }else{
-        res.render('stories/edit',{
-            story,
-        })
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+    
+        if(!story){
+            return res.render('error/404')
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        }else{
+            res.render('stories/edit',{
+                story,
+            })
+        }
+    } catch (error) {
+        console.log(err)
+        res.render('error/500')
     }
 })
 
@@ -65,21 +68,39 @@ router.get('/edit/:id', ensureAuth ,async (req,res)=>{
 // @desc Upadte story
 // @route PUT /stories/:id
 router.put('/:id', ensureAuth , async (req,res)=>{
-    let story = await Story.findById(req.params.id).lean()
+    try {
+        let story = await Story.findById(req.params.id).lean()
 
-    if(!story){
-        return res.render('error/404')
+        if(!story){
+            return res.render('error/404')
+        }
+
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        }else{
+            story = await Story.findOneAndUpdate({_id: req.params.id},req.body,{
+                new: true,
+                runValidators: true
+            })
+
+            res.redirect('/dashboard')
+        }
+    } catch (error) {
+        console.log(err)
+        res.render('error/500')
     }
+})
 
-    if(story.user != req.user.id){
-        res.redirect('/stories')
-    }else{
-        story = await Story.findOneAndUpdate({_id: req.params.id},req.body,{
-            new: true,
-            runValidators: true
-        })
 
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth , async (req,res)=>{
+    try{
+        await Story.remove({_id: req.params.id})
         res.redirect('/dashboard')
+    }catch(err){
+        console.log(err)
+        res.render('error/500')
     }
 })
 
